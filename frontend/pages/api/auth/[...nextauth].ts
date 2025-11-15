2f04ddb58f89d16bd1f98f8786c58707320447cb
Developer <you@example.com>
2025-11-15 20:31:30 +0700

    fix nextauth config

--- frontend/pages/api/auth/[...nextauth].ts
+++ frontend/pages/api/auth/[...nextauth].ts
@@ -4,21 +4,30 @@ import GoogleProvider from "next-auth/providers/google";
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+
   pages: {
     signIn: "/auth/login",
   },
+
   session: {
     strategy: "jwt",
   },
+
   callbacks: {
     async session({ session, token }) {
       return session;
     },
+
+    async redirect({ url, baseUrl }) {
+      // redirect ke dashboard setelah login
+      return `${baseUrl}/dashboard`;
+    },
   },
+
   secret: process.env.NEXTAUTH_SECRET,
 };
 
